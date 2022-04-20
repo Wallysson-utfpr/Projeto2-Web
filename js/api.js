65521a4007c10606c32dcc57147c43f6ad2017b4
Wallysson Barbosa <73896874+Wallysson-utfpr@users.noreply.github.com>
2022-04-19 22:33:42 -0300

    19042022 - 22:33

--- js/api.js
+++ js/api.js
@@ -1,29 +1,54 @@
 const init = () => {
-        const inputEmail = document.querySelector('input[type = "email"]');
-        const inputPassword = document.querySelector('input[type = "password"]');
-        const submitButton = document.querySelector('.login-submit');
-    
-        if (submitButton) {
-            submitButton.addEventListener('click', (event) => {
-                event.preventDefault();
-                fetch('https://reqres.in/api/login', {
-                    method: 'POST',
-                    headers: {
-                        'content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        email: inputEmail.value,
-                        password: inputPassword.value,
-                    })
-                }).then((response) => {
-                    return response.json();
-                }).then((data) => {
-                    console.log(data);
-                })
-            })
-        }
-    }
-    
-  
-    window.onload = init;
+    const inputEmail = document.querySelector('input[type = "email"]');
+    const inputPassword = document.querySelector('input[type = "password"]');
+    const submitButton = document.querySelector('.login-submit');
 
+    if (submitButton) {
+        submitButton.addEventListener('click', (event) => {
+            event.preventDefault();
+            fetch('https://reqres.in/api/login', {
+                method: 'POST',
+                headers: {
+                    'content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: inputEmail.value,
+                    password: inputPassword.value,
+                })
+            }).then((response) => {
+                return response.json();
+            }).then((data) => {
+                console.log(data);
+
+                if (data.token === 'QpwL5tke4Pnpja7X4') {
+                    openModal('dv-modal2');
+                    getmoedas();
+                }
+            })
+
+        })
+    }
+}
+
+window.onload = init;
+
+function getmoedas() {
+    let nameusd = document.querySelector('.name-usd');
+    let altausd = document.querySelector('.alta-usd');
+    let baixausd = document.querySelector('.baixa-usd');
+
+    let nameeur = document.querySelector('.name-eur');
+    let altaeur = document.querySelector('.alta-eur');
+    let baixaeur = document.querySelector('.baixa-eur');
+
+    (async () => {
+        let json = await axios.get('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL');
+        nameusd.innerHTML = json.data.USDBRL.name;
+        altausd.innerHTML = json.data.USDBRL.high;
+        baixausd.innerHTML = json.data.USDBRL.low;
+
+        nameeur.innerHTML = json.data.EURBRL.name;
+        altaeur.innerHTML = json.data.EURBRL.high;
+        baixaeur.innerHTML = json.data.EURBRL.low;
+    })();
+}
