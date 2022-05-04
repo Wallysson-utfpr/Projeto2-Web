f5893d1b1fd785d11eed0962254f695b33464ec1
Wallysson Barbosa <73896874+Wallysson-utfpr@users.noreply.github.com>
2022-05-03 22:33:09 -0300

    Implementação lista api
    Implementação lista api

--- js/api.js
+++ js/api.js
@@ -1,4 +1,3 @@
-
 const init = () => {
     const inputEmail = document.querySelector('input[type = "email"]');
     const inputPassword = document.querySelector('input[type = "password"]');
@@ -38,23 +37,9 @@ window.onload = init;
 
 function getmoedas() {
 
-    let nameusd = document.querySelector('.name-usd');
-    let altausd = document.querySelector('.alta-usd');
-    let baixausd = document.querySelector('.baixa-usd');
-
-    let nameeur = document.querySelector('.name-eur');
-    let altaeur = document.querySelector('.alta-eur');
-    let baixaeur = document.querySelector('.baixa-eur');
-
-    let namebtc = document.querySelector('.name-btc');
-    let altabtc = document.querySelector('.alta-btc');
-    let baixabtc = document.querySelector('.baixa-btc');
-
     let nameescolha = document.querySelector('.name-escolha');
     let altaescolha = document.querySelector('.alta-escolha');
     let baixaescolha = document.querySelector('.baixa-escolha');
-
-
 
 
     (async () => {
@@ -62,33 +47,31 @@ function getmoedas() {
         let escolhatxt = document.querySelector('.escolha');
         var teste = escolhatxt.value;
         var teste2 = teste;
-        let json = await axios.get('https://economia.awesomeapi.com.br/last/' + teste);
+        var lista = document.querySelector("ul"),
+            input = document.querySelector("#teste");
 
+        (async () => {
 
-        //let url = 'https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL, ' = escolhatxt;
-        //let json = await axios.get(url);
-        /*
-        nameusd.innerHTML = json.data.USDBRL.name;
-        altausd.innerHTML = json.data.USDBRL.high;
-        baixausd.innerHTML = json.data.USDBRL.low;
+            let listamoedas = await axios.get('https://economia.awesomeapi.com.br/json/available');
 
-        nameeur.innerHTML = json.data.EURBRL.name;
-        altaeur.innerHTML = json.data.EURBRL.high;
-        baixaeur.innerHTML = json.data.EURBRL.low;
+            if (!listamoedas.data[teste]) {
+                var li = document.createElement("li");
+                li.innerHTML = "Ops! Moeda inexistente";
+                lista.appendChild(li).style.color = "yellow";
+            } else {
+                let json = await axios.get('https://economia.awesomeapi.com.br/last/' + teste);
 
-        namebtc.innerHTML = json.data.BTCBRL.name;
-        altabtc.innerHTML = json.data.BTCBRL.high;
-        baixabtc.innerHTML = json.data.BTCBRL.low;
-        */
-        teste2 = teste.replace("-", "");
+                teste2 = teste.replace("-", "");
 
-        nameescolha.innerHTML = json.data[teste2].name;
-        altaescolha.innerHTML = json.data[teste2].high;
-        baixaescolha.innerHTML = json.data[teste2].low;
-
+                nameescolha.innerHTML = json.data[teste2].name;
+                altaescolha.innerHTML = json.data[teste2].high;
+                baixaescolha.innerHTML = json.data[teste2].low;
+            }
+        })();
 
     })();
 }
+
 
 function showHide(id) {
 
@@ -96,7 +79,6 @@ function showHide(id) {
     conteudo.classList.toggle('ativo');
 
 }
-
 
 function logout() {
     localStorage.setItem("token", "undefined");
@@ -111,44 +93,44 @@ function recarregar() {
 }
 
 function verificatxt() {
-    
+
     var lista = document.querySelector("ul"),
-    input = document.querySelector("#teste");
+        input = document.querySelector("#teste");
 
     input.addEventListener("keyup", function () {
         var palavra = input.value;
         lista.innerHTML = "";
-            if (palavra.length < 1) {
-                var li = document.createElement("li");
-                li.innerHTML = "Não pode ser vazio...";
-                lista.appendChild(li).style.color = "red";
+        if (palavra.length < 1) {
+            var li = document.createElement("li");
+            li.innerHTML = "Não pode ser vazio...";
+            lista.appendChild(li).style.color = "red";
 
-            } else if (palavra.length >= 1 && palavra.length  <= 6) {
-                var li = document.createElement("li");
-                li.innerHTML = "Tá quase lá..";
-                lista.appendChild(li).style.color = "yellow";
+        } else if (palavra.length >= 1 && palavra.length <= 6) {
+            var li = document.createElement("li");
+            li.innerHTML = "Tá quase lá..";
+            lista.appendChild(li).style.color = "yellow";
 
-                var li = document.createElement("li");
-                li.innerHTML = "Formato esperado {XXX-XXX}";
-                lista.appendChild(li).style.color = "yellow";
-            
-            } else if (palavra.length > 7) {
-                var li = document.createElement("li");
-                li.innerHTML = "Ops! Tem mais digito que o permitido";
-                lista.appendChild(li).style.color = "yellow";
+            var li = document.createElement("li");
+            li.innerHTML = "Formato esperado {XXX-XXX}";
+            lista.appendChild(li).style.color = "yellow";
 
-                var li = document.createElement("li");
-                li.innerHTML = "O formato esperado {XXX-XXX}";
-                lista.appendChild(li).style.color = "yellow";
+        } else if (palavra.length > 7) {
+            var li = document.createElement("li");
+            li.innerHTML = "Ops! Tem mais digito que o permitido";
+            lista.appendChild(li).style.color = "yellow";
 
-            } else if (palavra.length == 7) {
-                var li = document.createElement("li");
-                li.innerHTML = "OK!";
-                lista.appendChild(li).style.color = "green";
+            var li = document.createElement("li");
+            li.innerHTML = "O formato esperado {XXX-XXX}";
+            lista.appendChild(li).style.color = "yellow";
 
-                var li = document.createElement("li");
-                li.innerHTML = palavra;
-                lista.appendChild(li).style.color = "green";
-            }
+        } else if (palavra.length == 7) {
+            var li = document.createElement("li");
+            li.innerHTML = "Tamanho OK!";
+            lista.appendChild(li).style.color = "green";
+
+            var li = document.createElement("li");
+            li.innerHTML = "Formato ok " + palavra;
+            lista.appendChild(li).style.color = "green";
+        }
     });
 }
