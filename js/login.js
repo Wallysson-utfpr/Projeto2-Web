38217441fb647a5dc47026e46b717ca17ac3aa1c
joseroberto <joseroberto@alunos.utfpr.edu.br>
2022-05-07 22:19:59 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,19 +1,19 @@
 function openModal(mn) {
     let modal = document.getElementById(mn);
-
-    if (typeof modal == 'undefined' || modal === null)
+    if (typeof modal == 'undefined' || modal === null){
         return;
-
-    modal.style.display = 'Block';
-    document.body.style.overflow = 'hidden';
+    }
+        modal.style.display = 'Block';
+        document.body.style.overflow = 'hidden';
 }
+
 
 function closeModal(mn) {
     let modal = document.getElementById(mn);
 
-    if (typeof modal == 'undefined' || modal === null)
+    if (typeof modal == 'undefined' || modal === null){
         return;
-
+    }
     modal.style.display = 'none';
     document.body.style.overflow = 'auto';
 }
